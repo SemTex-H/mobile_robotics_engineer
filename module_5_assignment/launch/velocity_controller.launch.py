601bad321f259c6b6d7ffa21c08317abc87a9dcc
SemTex-H <oyunchuluunganbold@gmail.com>
2025-08-03 12:16:06 +0800

    Add: module_5_assignment

---
+++ module_5_assignment/launch/velocity_controller.launch.py
@@ -0,0 +1,43 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+from launch.actions import ExecuteProcess
+
+def generate_launch_description():
+    pkg_path = get_package_share_directory('module_5_assingment')
+
+    urdf_file = os.path.join(pkg_path, 'urdf', 'diff_drive.urdf')
+    
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            arguments=[urdf_file]
+        ),
+        ExecuteProcess(
+            cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
+            output='screen'
+        ),
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            name='robot_spawner',
+            output='screen',
+            arguments=['-topic', '/robot_description', '-entity', 'diff_drive_bot']
+        ),
+        Node(
+            package='controller_manager',
+            executable='spawner',
+            output='screen',
+            arguments=['wheels_velocity_controller']
+        ),
+        Node(
+            package='controller_manager',
+            executable='spawner',
+            output='screen',
+            arguments=['joint_state_publisher']
+        ),
+    ])
